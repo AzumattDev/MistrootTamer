bb8b9898525075f9bdbbbecddb1d8001d4a3a690
AzumattDev <azumattgaming@gmail.com>
2024-04-20 07:58:59 -0400

    Tweak README.md and add code.

--- Plugin.cs
+++ Plugin.cs
@@ -16,7 +16,7 @@ namespace MistrootTamer
     public class MistrootTamerPlugin : BaseUnityPlugin
     {
         internal const string ModName = "MistrootTamer";
-        internal const string ModVersion = "1.0.0";
+        internal const string ModVersion = "1.0.1";
         internal const string Author = "Azumatt";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
